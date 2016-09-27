1071dd322ead9c6d79c830fb417bfed051a4ea2e
parth <prparikh02@gmail.com>
2016-09-27 02:21:16 -0400

    Added highways

--- src/services/generate_maps.py
+++ src/services/generate_maps.py
@@ -9,6 +9,7 @@ def generate_map(rows, cols):
 
     G = init_grid(rows, cols)
     add_hard_to_traverse_cells(G)
+    add_highways(G)
 
     data = json_graph.node_link_data(G)
     return json.dumps(data, indent=4, separators=(',', ': '))
@@ -16,10 +17,10 @@ def generate_map(rows, cols):
 def init_grid(rows, cols):
         
     G = nx.Graph(rows=rows, cols=cols)
-    # TODO: Checks for x,y
+    
     for i in range(cols):
         for j in range(rows):
-            G.add_node(to_node_name(i, j), cell_type='unblocked', x=i, y=j)
+            G.add_node(to_node_name(i, j), x=i, y=j, cell_type='unblocked', has_highway=False)
 
     # potential edges
     edge_dirs = [[-1, -1], [0, -1], [1, -1], [-1, 0],
@@ -42,11 +43,8 @@ def to_node_name(x, y):
 
 def add_hard_to_traverse_cells(G):
 
-    print G.graph['cols'], G.graph['rows']
-
     coords = [(random.randint(0,G.graph['cols']-1), 
                random.randint(0,G.graph['rows']-1)) for i in range(8)]
-    print coords
 
     # check 31x31 block centered at coords
     radius = 15
@@ -61,3 +59,84 @@ def add_hard_to_traverse_cells(G):
                     node = G.node[to_node_name(i, j)]
                     if node['cell_type'] != 'hard_to_traverse':
                         node['cell_type'] = 'hard_to_traverse'
+
+def add_highways(G):
+    
+    for i in range(4):
+        highways = []
+        while not highways:
+            highways = create_highway(G)
+
+        for x, y in highways:
+            G.node[to_node_name(x, y)]['has_highway'] = True
+
+def create_highway(G):
+    
+    highway = []
+    x, y, direction = get_highway_starting_point(G.graph['rows'], G.graph['cols'])
+    if G.node[to_node_name(x, y)]['has_highway']:
+        return []
+
+    highway = [(x, y)]
+    steps = 1
+
+    while True:
+        for i in range(20):
+            x, y = get_next_highway_point(x, y, direction)
+            if (G.node[to_node_name(x, y)]['has_highway'] or
+                (x, y) in highway):
+                return []
+            
+            if is_boundary_point(G, G.node[to_node_name(x, y)]):
+                if steps >= 100:
+                    highway.append((x, y))        
+                    return highway
+                return []
+
+            highway.append((x, y))
+            steps += 1
+
+        direction = choose_next_direction(direction)
+
+
+def choose_next_direction(curr_direction):
+    
+    if curr_direction in ['n', 's']:
+        perp_directions = ['e', 'w']
+    else:
+        perp_directions = ['n', 's']
+
+    if random.uniform(0, 1) < 0.60:
+        return curr_direction
+    return random.choice(perp_directions)
+
+def get_next_highway_point(x, y, direction):
+    
+    if direction == 'n':
+        return x, y+1
+    elif direction == 's':
+        return x, y-1
+    elif direction == 'e':
+        return x+1, y
+    else:
+        return x-1, y
+        
+
+def get_highway_starting_point(row_count, col_count):
+    
+    # starting point for the highway should be at boundary,
+    #   but not at the corners
+    side = random.choice(['n', 's', 'e', 'w'])
+    if side == 'n':
+        return random.randint(1, col_count-1), row_count-1, 's'
+    elif side == 's':
+        return random.randint(1, col_count-1), 0, 'n'
+    elif side == 'e':
+        return col_count-1, random.randint(1, row_count-1), 'w'
+    else:
+        return 0, random.randint(1, row_count-1), 'e'
+
+def is_boundary_point(G, n):
+    
+    return (n['x'] in [0, G.graph['cols']-1] or 
+            n['y'] in [0, G.graph['rows']-1])
